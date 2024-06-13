c449f5300e25b118ab002cfbf40ad24d30868bbb
Alexis <alexisflorentin14@gmail.com>
2024-06-13 17:02:56 +0200

    add functions to client class and organise file

--- includes/Client.hpp
+++ includes/Client.hpp
@@ -6,7 +6,7 @@
 /*   By: alfloren <alfloren@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 09:48:14 by alfloren          #+#    #+#             */
-/*   Updated: 2024/06/13 15:55:14 by alfloren         ###   ########.fr       */
+/*   Updated: 2024/06/13 17:01:40 by alfloren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,19 +23,41 @@ class Client
 		// Client(const Client &other);
 		~Client();
 		// Client &operator=(const Client &other);
-
+		
+		//---------Getters------------
 		int			getFd();
+		std::string	getNickname();
+		std::string	getPassword();
+		bool		getIsOperator();
+		std::string	getIpAdd();
+		std::string	getUsername();
+		std::string	getBuffer();
+
+		//---------Setters------------
 		void		setFd(int fd);
 		void		setIpAdd(std::string ipadd);
-		std::string	getBuffer();
 		void		setBuffer(std::string buffer);
+		void		setNickname(std::string nickname);
+		void		setPassword(std::string password);
+		void		setIsOperator(bool is_admin);
+		void		setIpAdd(std::string ip_add);
+		void		setUsername(std::string username);
+
+		//---------Methods------------
+		void		sendMessage(const std::string message);
+		void		addInvolvedChannel(std::string channelName);
+		void		removeInvolvedChannel(std::string channelName);
 	private:
 		int				_fd;
+		std::string		_username;
 		std::string		_nickname;
 		std::string		_password;
 		std::string		_ip_add;
-		bool			_is_admin;
+		bool			_is_operator;
+		bool			_registered;
+		bool			_logged;
 		std::string		_buffer;
+		std::string		_channelInvolved;
 		// struct sockaddr_in	_cliaddr;
 };
 
--- src/Client.cpp
+++ src/Client.cpp
@@ -6,7 +6,7 @@
 /*   By: alfloren <alfloren@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 09:48:28 by alfloren          #+#    #+#             */
-/*   Updated: 2024/06/13 16:08:27 by alfloren         ###   ########.fr       */
+/*   Updated: 2024/06/13 17:01:07 by alfloren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 Client::Client()
 {
-	_is_admin = false;
+	_is_operator = false;
 	// std::cout << VERT << "Default constructor are called" << REINIT << std::endl;
 }
 
@@ -23,41 +23,52 @@ Client::Client(int fd, std::string nickname, std::string password, std::string i
 		_nickname(nickname),
 		_password(password),
 		_ip_add(ipadd),
-		_is_admin(false)
+		_is_operator(false)
 {
 	// std::cout << VERT << "Constructor are called" << REINIT << std::endl;
 };
 
-Client::~Client()
+Client::~Client(){}
+
+Client &Client::operator=(const Client &other)
 {
+	if (this != &other)
+	{
+		this->_fd = other._fd;
+		this->_nickname = other._nickname;
+		this->_password = other._password;
+		this->_ip_add = other._ip_add;
+		this->_is_operator = other._is_operator;
+	}
+	return (*this);
 }
 
-// Client &Client::operator=(const Client &other)
-// {
-// }
+//----------------Getters------------
+int	Client::getFd() {return (this->_fd);}
+std::string	Client::getNickname() {return (this->_nickname);}
+std::string Client::getBuffer() {return (this->_buffer);}
+std::string	Client::getIpAdd() {return (this->_ip_add);}
+std::string	Client::getUsername() {return (this->_username);}
+std::string	Client::getPassword() {return (this->_password);}
+bool Client::getIsOperator() {return (this->_is_operator);}
 
-int	Client::getFd()
-{
-	return (this->_fd);
-}
+//----------------Setters------------
+void	Client::setFd(int fd) {this->_fd = fd;}
+void	Client::setBuffer(std::string buffer) {this->_buffer += buffer;}
+void	Client::setIpAdd(std::string ipadd) {this->_ip_add = ipadd;}
+void	Client::setNickname(std::string nickname) {this->_nickname = nickname;}
+void	Client::setPassword(std::string password) {this->_password = password;}
+void	Client::setIsOperator(bool is_admin) {this->_is_operator = is_admin;}
+void	Client::setIpAdd(std::string ip_add) {this->_ip_add = ip_add;}
+void	Client::setUsername(std::string username) {this->_username = username;}
 
-void	Client::setFd(int fd)
-{
-	this->_fd = fd;
-}
+//----------------Methods------------
+void	Client::sendMessage(const std::string message)
+{std::cout << "Message sent: " << message << std::endl;}
 
-std::string Client::getBuffer()
-{
-	return (this->_buffer);
-}
+void	Client::addInvolvedChannel(std::string channelName)
+{}
 
-void	Client::setBuffer(std::string buffer)
-{
-	this->_buffer += buffer;
-}
-
-void	Client::setIpAdd(std::string ipadd)
-{
-	this->_ip_add = ipadd;
-}
+void	Client::removeInvolvedChannel(std::string channelName)
+{}
 
