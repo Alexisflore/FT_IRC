46a107b730b23de48fdeb54f3bca4f8c6424f940
jferment <julien.fv@live.fr>
2024-06-13 21:35:11 +0200

    correction part et quit

--- src/process/part.cpp
+++ src/process/part.cpp
@@ -12,10 +12,30 @@
 
 #include "../../includes/Server.hpp"
 
-void Server::processPass(int fd, std::vector <std::string> string)
+void Server::processPass(int fd, std::vector <std::string> args)
 {
-		(void)string;
-	std::string		msg = "PART\n";
+		if (args.size() > 2)
+		std::cout << "Usage : \"QUIT nameofthechannel\"" << std::endl; // a envoyer au client a la place de l ecrire cote serveur
+	else if (args.size() == 2)
+	{
+		std::cout << "Client " << fd << " is trying to quit channel #" << args[1] << std::endl;
+		std::string channelName = args[1];
 
-	send(fd, msg.c_str(), msg.length(), 0);
+		std::vector<Channel>::iterator it = std::find_if(
+			this->_channels.begin(),
+			this->_channels.end(),
+			ChannelNameComparator(channelName));
+		if (it != this->_channels.end()) // le canal existe
+		{
+			Channel& channel = *it;
+			if (channel.isClientInChannel(fd))
+				channel.leaveChannel(fd);
+			else
+				std::cout << "Client " << fd << " isn't in the channel " << channelName << std::endl;
+		}
+		else 
+			std::cout << "Channel " << channelName << " doesnt exist yet!" << std::endl;
+	}
+	else
+		std::cout << "need to specify the channel name" << std::endl;
 }
--- src/process/quit.cpp
+++ src/process/quit.cpp
@@ -12,30 +12,10 @@
 
 #include "../../includes/Server.hpp"
 
-void	Server::processQuit(int fd, std::vector<std::string> args)
+void	Server::processQuit(int fd, std::vector<std::string> string)
 {
-	if (args.size() > 2)
-		std::cout << "Usage : \"QUIT nameofthechannel\"" << std::endl; // a envoyer au client a la place de l ecrire cote serveur
-	else if (args.size() == 2)
-	{
-		std::cout << "Client " << fd << " is trying to quit channel #" << args[1] << std::endl;
-		std::string channelName = args[1];
+	(void)string;
+	std::string		msg = "PART\n";
 
-		std::vector<Channel>::iterator it = std::find_if(
-			this->_channels.begin(),
-			this->_channels.end(),
-			ChannelNameComparator(channelName));
-		if (it != this->_channels.end()) // le canal existe
-		{
-			Channel& channel = *it;
-			if (channel.isClientInChannel(fd))
-				channel.leaveChannel(fd);
-			else
-				std::cout << "Client " << fd << " isn't in the channel " << channelName << std::endl;
-		}
-		else 
-			std::cout << "Channel " << channelName << " doesnt exist yet!" << std::endl;
-	}
-	else
-		std::cout << "need to specify the channel name" << std::endl;
+	send(fd, msg.c_str(), msg.length(), 0);
 }
