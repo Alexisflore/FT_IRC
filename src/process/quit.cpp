75f26fcade8b4564473b8450be9ccce0dffd6315
jferment <julien.fv@live.fr>
2024-06-22 18:18:18 +0200

    quit

--- src/process/quit.cpp
+++ src/process/quit.cpp
@@ -14,5 +14,70 @@
 
 void	Server::processQuit(int fd, std::string arg)
 {
+	std::vector<std::string> args = split_args(arg, " ");
+	std::string quitMessage;
 
+	if (args.size() > 1)
+	{
+		quitMessage = args[1];
+		for (size_t i = 2; i < args.size(); i++)
+		{
+			quitMessage += " " + args[i];
+		}
+	}
+	else 
+	{
+		for (size_t i = 0; i < this->_clients.size(); i++)
+		{
+			if (_clients[i].getFd() == fd)
+			{
+				quitMessage = _clients[i].getNickname();
+			}
+			break;
+		}
+	}
+
+	std::cout << "Client " << fd << " has quit: " << quitMessage << std::endl;
+
+	closeConnection(fd, quitMessage);
+}
+
+void	Server::closeConnection(int fd, std::string quitMessage)
+{
+	std::stringstream ss;
+	ss << fd;
+	std::string fdStr = ss.str();
+
+	std::string disconectMessage = "Client " + fdStr + " has quit: " + quitMessage;
+	for (size_t i = 0; i < this->_clients.size(); i++)
+	{
+		if (_clients[i].getFd() != fd)
+			send(_clients[i].getFd(), disconectMessage.c_str(), disconectMessage.size(), 0);
+	}
+	
+	for (size_t i = 0; i < _channels.size(); i++)
+	{
+		_channels[i].leaveChannel(fd);
+	}
+
+	close(fd);
+
+	for (size_t i = 0; i < _clients.size(); i++)
+	{
+		if (_clients[i].getFd() == fd)
+		{
+			_clients.erase(_clients.begin() + i);
+			break;
+		}
+	}
+	
+	for (size_t i = 0; i < _fds.size(); i++)
+	{
+		if (_fds[i].fd == fd)
+		{
+			_fds.erase(_fds.begin() + i);
+			break;
+		}
+	}
+	
 }
